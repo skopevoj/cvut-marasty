02b0ff4526119047b1fa6425a197f74e8b765098
Vojtěch Škopek <39000132+skopevoj@users.noreply.github.com>
2026-01-07 23:55:40 +0100

    refactor: remove subjects from public

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QuizProvider } from "./lib/QuizContext";
+import { StatsProvider } from "./lib/StatsContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QuizProvider>
-          {children}
-        </QuizProvider>
+        <StatsProvider>
+          <QuizProvider>
+            {children}
+          </QuizProvider>
+        </StatsProvider>
       </body>
     </html>
   );
